2ee8b73eabe614095c11a74120446a8b179423d4
Tokolo || Data || NLP <36508704+Ndiya@users.noreply.github.com>
2021-09-06 07:53:44 +0200

    new commit----changes made
    new commit----changes made

--- dash-nlp/app.py
+++ dash-nlp/app.py
@@ -347,6 +347,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__,server=server,external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 server = app.server
 
 
